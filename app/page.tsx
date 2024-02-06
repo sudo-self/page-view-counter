8b453bcecfb3c75debdcd8cbe73a4e9cf3b4ff6f
Sudo-Self <sudo-self@proton.me>
2024-02-06 09:44:08 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-slate/50 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
         <p>
-    <!-- Place this tag where you want the button to render. -->
-<GitHubButton href="https://github.com/sudo-self/page-view-counter" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star sudo-self/page-view-counter on GitHub">Star</GitHubButton>
+          Refresh Page
+
         </p>
         <ExpandingArrow />
       </Link>
@@ -35,7 +35,7 @@ export default function Home() {
                ᗧ···ᗣ···ᗣ····♥···ᗣ····
             </p>
           </div>
-       <p>Vercel kv + Redis </p>
+       <p>Vercel kv + Redis&nbsp;<GitHubButton href="https://github.com/sudo-self/page-view-counter" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star sudo-self/page-view-counter on GitHub">Star</GitHubButton></p>
         </div>
         <div className="flex flex-col text-emerald-800 space-y-4">
           <center><iframe src="https://app-svelte-eight.vercel.app/" scrolling="no" height="300px" width="300px"></iframe></center>
