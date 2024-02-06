bbe7626aa8ad01c088f7c8c2bc1fa39d57ba4364
Sudo-Self <sudo-self@proton.me>
2024-02-05 22:43:45 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="relative flex min-h-screen bg-gray-300 flex-col items-center justify-center">
       <Link
-        href="https://views.jessejesse.com"
+        href="https://page-views.vercel.app/"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-slate/50 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
         <p>
